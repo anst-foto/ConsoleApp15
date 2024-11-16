f5d3d4ac8e1c5ec5e54ff46930dbcb2e34661b36
Starinin Andrey <starinin-andrey@ya.ru>
2024-11-16 19:27:35 +0300

    Initial commit

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1;
+
+public class Contacts
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+}
+
+public class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public Contacts Contacts { get; set; }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using ConsoleApp1;
+using Npgsql;
+
+const string connectionString =
+    "Server=localhost;Port=5432;Database=test_db;User Id=postgres;Password=1234;Search Path=public";
+var db = new NpgsqlConnection(connectionString);
+
+const string sql = """
+                   SELECT table_persons.id AS id,
+                          table_persons.name AS name,
+                          table_contacts.email AS email,
+                          table_contacts.phone AS phone
+                   FROM table_persons
+                       JOIN table_contacts 
+                           ON table_persons.id = table_contacts.person_id;
+                   """;
+db.Open();
+var command = new NpgsqlCommand(sql, db);
+var result = command.ExecuteReader();
+var persons = new List<Person>();
+if (result.HasRows)
+{
+    while (result.Read())
+    {
+        var person = new Person
+        {
+            Id = result.GetInt32("id"),
+            Name = result.GetString("name"),
+            Contacts = new Contacts
+            {
+                Email = result.GetString("email"),
+                Phone = result.GetString("phone")
+            }
+        };
+        persons.Add(person);
+    }
+}
+db.Close();
+
+foreach (var person in persons)
+{
+    Console.WriteLine($"{person.Id} \t {person.Name} \t {person.Contacts.Email} \t {person.Contacts.Phone}");
+}
